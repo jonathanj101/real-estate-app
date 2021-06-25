7f1359a9578c419e0a6ad1141d037f90b88dcee1
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-25 12:48:04 -0400

    following yt tutorial need help understanding serializers django

--- server/api/views.py
+++ server/api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializer import UserSerializer
 from .models import User
 
@@ -9,3 +11,10 @@ from .models import User
 class UserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    # print(request.data)
+
+    # def post(self, request, format=None):
+    #     if not self.request.session.exists(self.request.session.session_key()):
+    #         self.request.session.create()
+    #     serializer = self.serializer_class(data=request.data)
+    #     if serializer.is_valid():
