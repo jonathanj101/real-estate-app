2a795cfa35994e00e88d2face56acc4eb7601bb8
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-24 23:50:15 -0400

    hmm testing how django works

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -8,7 +8,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>Testing React code</h1>
+                <h1>maybe</h1>
             </div>
         );
     }
