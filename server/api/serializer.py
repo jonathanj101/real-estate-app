f47043e03e5829afca0a7cda045804ce86a16660
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-25 12:44:32 -0400

    just testing how django works, still need help

--- server/api/serializer.py
+++ server/api/serializer.py
@@ -5,5 +5,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", 'first_name', 'last_name',
-                  'username', "password", 'email')
+        fields = ("id", 'username', 'email')
