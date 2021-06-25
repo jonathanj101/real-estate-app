6a26efc034a91b975ea1173b9fb9cd78cefed099
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-25 00:53:26 -0400

    Testing react routing with django

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import Home from "./Home";
 
 export default class App extends Component {
     constructor(props) {
@@ -8,7 +9,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>maybe</h1>
+                <Home />
             </div>
         );
     }
