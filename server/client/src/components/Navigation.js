5764024c719beb59b6ebb61813105a17f27f79c4
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-25 22:08:56 -0400

    testing, css file not updating?

--- server/client/src/components/Navigation.js
+++ server/client/src/components/Navigation.js
@@ -1,18 +1,44 @@
-import React from "react";
-import { AppBar, Tabs, Tab } from "@material-ui/core";
+import React, { useState } from "react";
+import { AppBar, Tabs, Tab, Link, Typography, makeStyles } from "@material-ui/core";
+import { red } from "@material-ui/core/colors";
 
 function Navigation() {
+    const [value, setValue] = useState("");
+
+    const handleChange = (event, newValue) => {
+        console.log(`event ${event} & newvalue ${newValue}`);
+    };
+
+    const classes = useStyles();
     return (
         <div>
-            <AppBar position="static">
-                <Tabs aria-label="simple tabs example">
-                    <Tab label="Item One" />
-                    <Tab label="Item Two" />
-                    <Tab label="Item Three" />
+            <AppBar className={classes.navBarWidth} position="static">
+                <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+                    <Tab href="/" label="Home" />
+                    <Tab href="/about" label="About" />
+                    <Tab href="/example" label="Example" />
                 </Tabs>
             </AppBar>
         </div>
     );
 }
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: theme.spacing(1),
+        [theme.breakpoints.down("sm")]: {
+            backgroundColor: theme.palette.secondary.main,
+        },
+        [theme.breakpoints.up("md")]: {
+            backgroundColor: theme.palette.primary.main,
+        },
+        [theme.breakpoints.up("lg")]: {
+            backgroundColor: red[500],
+        },
+    },
+    navBarWidth: {
+        width: "100% !important",
+    },
+}));
 
 export default Navigation;
