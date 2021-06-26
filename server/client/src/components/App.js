1d3b18120810fb61cf23d79bc00c8976e2927df4
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-25 23:23:25 -0400

    App will be main component of this app

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import Main from "./Main";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navigation from "./Navigation";
+import Footer from "./Footer";
+import Home from "../pages/Home";
+import About from "../pages/About";
 
 export default class App extends Component {
     constructor(props) {
@@ -9,9 +12,16 @@ export default class App extends Component {
     }
     render() {
         return (
-            <Router>
-                <Main />
-            </Router>
+            <div>
+                <Navigation />
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={() => <Home />} />
+                        <Route exact path="/about" component={() => <About />} />
+                    </Switch>
+                </Router>
+                <Footer />
+            </div>
         );
     }
 }
