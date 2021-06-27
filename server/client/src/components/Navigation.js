8e59bfa17fb836fc7a12c3f1547444717fd72c62
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-27 01:43:22 -0400

    link works perfectly, avoid react-router error

--- server/client/src/components/Navigation.js
+++ server/client/src/components/Navigation.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
-import { AppBar, Tabs, Tab, Link, Typography, makeStyles } from "@material-ui/core";
+import { Redirect } from "react-router";
+import { Link } from "react-router-dom";
+import { AppBar, Tabs, Tab, Typography, makeStyles, BottomNavigation, Button } from "@material-ui/core";
 
 function Navigation() {
     const [value, setValue] = useState("");
 
     const handleChange = (event, newValue) => {
         console.log(`event ${event} & newvalue ${newValue}`);
+        setValue(newValue);
+    };
+
+    const preventDefault = (e) => {
+        console.log(e);
+        // e.preventDefault();
+        console.log("prevented reload");
     };
 
     const classes = useStyles();
@@ -13,10 +22,31 @@ function Navigation() {
         <div>
             <AppBar position="static">
                 <div className={classes.navTabsDiv}>
-                    <Tabs id="tabs" value={value} onChange={handleChange} aria-label="simple tabs example">
-                        <Tab path="/" label="Home" />
-                        <Tab path="/about" label="About" />
+                    <Tabs
+                        value={value}
+                        id="tabs"
+                        value={value}
+                        onChange={handleChange}
+                        aria-label="simple tabs example"
+                    >
+                        <Tab label="Home" to="/home" />
+                        <Tab onClick={preventDefault} label="About" />
+                        <Button onClick={preventDefault}>About</Button>
+                        <Link onClick={preventDefault} to="/about">
+                            about
+                        </Link>
                     </Tabs>
+                    {/* <BottomNavigation value={value} onChange={handleChange} className={classes.root}> */}
+                    {/* <Link to="/about">About</Link> */}
+                    {/* </BottomNavigation> */}
+                    {/* <Tabs>
+                        <Link className={classes.linkStyles} href="/" label="Home">
+                            Home
+                        </Link>
+                        <Link href="/about" label="About">
+                            About
+                        </Link>
+                    </Tabs> */}
                     {/* <Tabs id="tabs" value={value} onChange={handleChange} aria-label="simple tabs example">
                         <Tab href="/" label="Home" />
                         <Tab href="/about" label="About" />
@@ -32,6 +62,14 @@ const useStyles = makeStyles({
     navTabsDiv: {
         display: "flex",
         justifyContent: "space-around",
+    },
+    linkStyles: {
+        border: "0",
+        cursor: "pointer",
+        margin: "0",
+        borderRadius: "0",
+        backgroundColor: "transparent",
+        cursor: "pointer",
     },
 });
 
