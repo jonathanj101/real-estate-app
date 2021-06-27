25d14333c697772960af578dc2a7f5de4c76faed
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-27 01:41:44 -0400

    made module only serving <main/>

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,31 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navigation from "./Navigation";
-import Footer from "./Footer";
-import Home from "../pages/Home";
-import About from "../pages/About";
+import { BrowserRouter } from "react-router-dom";
+import Main from "./Main";
 
-export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
+function App() {
+    return (
+        <BrowserRouter>
             <div>
-                <Navigation />
-                <Router>
-                    <Switch>
-                        <Route exact path="/" component={() => <Home />} />
-                        <Route exact path="/about" component={() => <About />} />
-                    </Switch>
-                </Router>
-                <Footer />
+                <Main />
             </div>
-        );
-    }
+        </BrowserRouter>
+    );
 }
-
 const appDiv = document.getElementById("app");
 
 render(<App />, appDiv);
