a0c7386526682dc53812732b748a4759a76accc9
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-27 01:42:39 -0400

    parent comp for other comp, wrapping comps to prevent react-router error

---
+++ server/client/src/components/Main.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Navigation from "./Navigation";
+import Footer from "./Footer";
+import Home from "../pages/Home";
+import About from "../pages/About";
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <div>
+                <Navigation />
+                <Switch>
+                    <Route exact path="/" render={() => <Home />} />
+                    <Route exact path="/about" render={() => <About />} />
+                </Switch>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default Main;
