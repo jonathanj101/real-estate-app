b92496711d612df58c556fad6ed8788c0a529e0a
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-30 22:43:33 -0400

    trying to understand better serializers

--- server/api/views.py
+++ server/api/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework import generics, status
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .serializer import UserSerializer
 from .models import User
 
