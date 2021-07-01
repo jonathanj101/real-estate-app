c0967366930ef752a1cd0799a8efb89e3a35cc66
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-30 22:44:58 -0400

    second way of doing navigation between pages

--- server/client/src/components/Navigation.js
+++ server/client/src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { AppBar, makeStyles, BottomNavigation, Button } from "@material-ui/core";
+import { AppBar, makeStyles, BottomNavigation, BottomNavigationAction, Button } from "@material-ui/core";
 import { Home, BusinessCenter } from "@material-ui/icons";
 
 function Navigation() {
@@ -9,28 +9,62 @@ function Navigation() {
 
     const handleChange = (event, newValue) => {
         console.log(`event ${event} & newvalue ${newValue}`);
-        console.log(value.currentTarget);
         setValue(newValue);
     };
 
     const redirectToPage = (e) => {
-        const page = e.currentTarget.attributes[3].textContent;
+        const page = e.currentTarget.children[0].children[0].attributes[4].nodeValue;
+        // console.log(value);
+        // const page = e.currentTarget.attributes[3].textContent;
         history.push(`${page}`);
+    };
+
+    const fetchingData = () => {
+        fetch("/example", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ something: "post request from react" }),
+        })
+            .then((resp) => resp.json())
+            .then((data) => console.log(data));
     };
 
     const classes = styles();
     return (
         <div>
-            <AppBar position="static" color="secondary">
+            <AppBar position="static" className={classes.nav}>
                 <div className={classes.navTabsDiv}>
-                    <BottomNavigation className={classes.btnNavigation}>
-                        <Button value="/" endIcon onClick={(e) => redirectToPage(e)}>
+                    <BottomNavigation
+                        value={value}
+                        className={classes.btnNavigation}
+                        onChange={handleChange}
+                        showLabels
+                    >
+                        <BottomNavigationAction
+                            className={history.location.pathname == "/" ? "Mui-selected" : classes.btnNavigationItems}
+                            onClick={redirectToPage}
+                            label="Home"
+                            icon={<Home value="/" />}
+                        />
+                        <BottomNavigationAction
+                            className={
+                                history.location.pathname == "/about" ? "Mui-selected" : classes.btnNavigationItems
+                            }
+                            onClick={redirectToPage}
+                            label="About"
+                            icon={<BusinessCenter value="/about" />}
+                        />
+                        <BottomNavigationAction />
+                        {/* <Button value="/" endIcon onClick={(e) => redirectToPage(e)}>
                             <div className={classes.btnNavigationItems}>
                                 <div>
                                     <Home />
                                 </div>
+                                Home
                             </div>
-                            Home
                         </Button>
                         <Button value="/about" endIcon onClick={(e) => redirectToPage(e)}>
                             <div className={classes.btnNavigationItems}>
@@ -40,6 +74,14 @@ function Navigation() {
                                 About us
                             </div>
                         </Button>
+                        <Button value="/example" endIcon onClick={fetchingData}>
+                            <div className={classes.btnNavigationItems}>
+                                <div>
+                                    <BusinessCenter />
+                                </div>
+                                example
+                            </div>
+                        </Button> */}
                     </BottomNavigation>
                 </div>
             </AppBar>
@@ -66,6 +108,16 @@ const styles = makeStyles({
     btnNavigationItems: {
         display: "flex",
         alignItems: "center",
+        color: "white",
+        "&:hover": {
+            color: "black",
+        },
+        ".Mui-selected": {
+            color: "black",
+        },
+    },
+    nav: {
+        backgroundColor: "red",
     },
 });
 
