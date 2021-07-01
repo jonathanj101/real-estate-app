df4feb68a42efc06643596475bb0ce151ebac196
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-30 22:45:40 -0400

    imported Search component

--- server/client/src/pages/Home.js
+++ server/client/src/pages/Home.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core";
 import Cards from "../components/Cards";
+import SearchComponent from "../components/SearchComponent";
 
 function Home() {
     return (
         <div>
+            <SearchComponent />
             <Cards />
         </div>
     );
 }
+
+// const styles = makeStyles({});
 
 export default Home;
