f693e8758eb6246e2d273ca375e01e8846978b85
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-06-30 22:47:52 -0400

    testing endpoint with api_view http response

--- server/client/views.py
+++ server/client/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 
+from rest_framework import generics, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
 # Create your views here.
 
 
 def index(request, *args, **kwargs):
     return render(request, 'frontend/index.html')
+
+
+@api_view(["GET", "POST"])
+def apiOverview(request):
+    print("apioverview")
+    if request.method == "POST":
+        print(request)
+        print(request.data)
+        return Response("Hello world")
+    return Response("get request")
