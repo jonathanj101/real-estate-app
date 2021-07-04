919b8ef40f5b5058d974126b04d05081ffad6a6d
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-07-03 23:51:48 -0400

    card display data correctly, but expand all  of the cards

--- server/client/src/components/Cards.js
+++ server/client/src/components/Cards.js
@@ -8,6 +8,7 @@ import {
     Typography,
     CardMedia,
     makeStyles,
+    Button,
 } from "@material-ui/core";
 import { Favorite, Share, ExpandMore } from "@material-ui/icons";
 // import house from "../../static/images/house.jpg";
@@ -15,10 +16,22 @@ import { Favorite, Share, ExpandMore } from "@material-ui/icons";
 const Cards = () => {
     const [expanded, setExpanded] = useState(false);
     const [isIconClick, setIconClick] = useState(false);
+    const [arr, setArr] = useState([]);
+
+    const fetchingData = () => {
+        fetch("/testing")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data.data);
+                setArr(data.data.props);
+            });
+    };
+    // console.log(arr.props);
 
     const classes = styles();
 
-    const handleExpandClick = () => {
+    const handleExpandClick = (index) => {
+        console.log(index);
         setExpanded(!expanded);
     };
     const handleIconOnClick = (e) => {
@@ -31,10 +44,50 @@ const Cards = () => {
             setIconClick(false);
         }
     };
+    const cardBody = arr.map((property, num) => {
+        // console.log(index);
+        return (
+            <Card key={num}>
+                <CardMedia className={classes.image} image={property.imgSrc} />
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary">
+                        <h2>{property.price}</h2>
+                    </Typography>
+                    <Typography>
+                        <span>{property.bathrooms}</span>
+                        <span>{property.propertyType}</span>
+                        <span>
+                            {property.lotAreaValue} | {property.lotAreaUnit}
+                        </span>
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <IconButton aria-label="add to favorites" onClick={handleIconOnClick}>
+                        <Favorite />
+                    </IconButton>
+                    <IconButton aria-label="share">
+                        <Share />
+                    </IconButton>
+                    <IconButton aria-label="show more" aria-expanded={expanded} onClick={() => handleExpandClick()}>
+                        <ExpandMore />
+                    </IconButton>
+                </CardActions>
+                <Collapse key={num} in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <Typography>
+                            GOOGLE MINI-MAP MIGHT GO IN THIS PLACE! TO DISPLAY PROPERTIES LOCATION!!
+                        </Typography>
+                    </CardContent>
+                </Collapse>
+            </Card>
+        );
+    });
 
     return (
         <div className={classes.mainDiv}>
-            <Card>
+            <Button onClick={fetchingData}>click here</Button>
+            <Card>{cardBody}</Card>
+            {/* <Card>
                 <CardMedia className={classes.image} image="../../static/images/house.jpg" />
                 <CardContent>
                     <Typography variant="body2" color="textSecondary">
@@ -59,14 +112,18 @@ const Cards = () => {
                         <Typography>Renovations if any</Typography>
                     </CardContent>
                 </Collapse>
-            </Card>
+            </Card> */}
         </div>
     );
 };
 
 const styles = makeStyles({
     mainDiv: {
-        width: 345,
+        display: "flex",
+        flexWrap: "wrap",
+        border: "10px solid black",
+        height: "100%",
+        width: "100%",
     },
     cardStyles: {},
     image: {
