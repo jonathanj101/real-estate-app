47ea99a7f15bd8fb4ff52925b3d5577d7470394f
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-07-03 23:52:31 -0400

    move fetching /testing to card.js

--- server/client/src/pages/Home.js
+++ server/client/src/pages/Home.js
@@ -1,13 +1,25 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core";
+import React, { useState } from "react";
+import { makeStyles, Button } from "@material-ui/core";
 import Cards from "../components/Cards";
 import SearchComponent from "../components/SearchComponent";
 
 function Home() {
+    // const [arr, setArr] = useState([]);
+
+    // const fetchingData = () => {
+    //     fetch("/testing")
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             console.log(data.data);
+    //             setArr(data.data.props);
+    //         });
+    // };
+
     return (
         <div>
             <SearchComponent />
             <Cards />
+            {/* <Button onClick={fetchingData}>click here</Button> */}
         </div>
     );
 }
