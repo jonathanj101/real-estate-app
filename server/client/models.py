d11deb764895f1af18929d0c20555013026b4248
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-07-05 22:25:25 -0400

    mocking api model, need practice and work

--- server/client/models.py
+++ server/client/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    # need to modify user model
+    first_name = models.CharField(max_length=20, default="", null=False)
+    last_name = models.CharField(max_length=20, default="", null=False)
+    username = models.CharField(
+        max_length=20, default="", null=False, unique=True)
+    password = models.CharField(
+        max_length=20, default="", null=False, unique=False)
+    email = models.EmailField(max_length=50, null=False, unique=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Property(models.Model):
+    address =  # need to add model field
+    price =  # need to add model field
+    property_type =  # need to add model field
+    bathrooms =  # need to add model field
+    bedrooms =  # need to add model field
+    sqft =  # need to add model field
+    zpid =  # need to add model field
+    latitude =  # need to add model field
+    longitude =  # need to add model field
