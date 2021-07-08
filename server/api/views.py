dbf2812774129a1812fb1fcc8f0049d84761aea8
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-07-07 22:16:01 -0400

    created views

--- server/api/views.py
+++ server/api/views.py
@@ -1,22 +1,62 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework import generics, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializer import UserSerializer
-from .models import User
+from .serializer import UserSerializer, PropertySerializer
+from .models import User, Property
 
 # Create your views here.
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-class UserView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    # print(request.data)
 
-    # def post(self, request, format=None):
-    #     if not self.request.session.exists(self.request.session.session_key()):
-    #         self.request.session.create()
-    #     serializer = self.serializer_class(data=request.data)
-    #     if serializer.is_valid():
+@api_view(["GET", "POST"])
+def apiOverview(request):
+    print("apioverview")
+    if request.method == "POST":
+        print(request)
+        print(request.data)
+        return Response("Hello world")
+    return Response("get request")
+
+
+@api_view(["GET"])
+def user_Account(request):
+    print("user account route")
+    return Response("user accout route")
+
+
+@api_view(["GET"])
+def test_zillow_api(request):
+
+    url = "https://zillow-com1.p.rapidapi.com/propertyExtendedSearch"
+
+    querystring = {"location": "santa monica, ca", "home_type": "Houses"}
+
+    headers = {
+        'x-rapidapi-key': "d9c46d06bcmsh8f8d34a1e32e159p124447jsna5a4785d45eb",
+        'x-rapidapi-host': "zillow-com1.p.rapidapi.com"
+    }
+
+    response = requests.get(url=url, headers=headers, params=querystring)
+    resp = response.json()
+    # print(resp)
+
+    # print(response.text)
+    return Response({"data": resp})
+
+
+@api_view(["GET"])
+def testing(request):
+    data = {"address": "123 fort", "price": 5000, "property_type": "single family", "bathrooms": 5, "bedrooms": 5,
+            "sqft": 5000, "zpid": 10032, "latitude": 50083.9, "longitude": 5000.5, "photo_main": "image-src.png"}
+    property_model = Property(address=data["address"], price=data["price"], property_type=data["property_type"], bathrooms=data["bathrooms"], bedrooms=data["bedrooms"],
+                              sqft=data["sqft"], zpid=data["zpid"], latitude=data["latitude"], longitude=data["longitude"], photo_main=data["photo_main"])
+    property_model.save()
+    queryset = Property.objects.all()
+    print(queryset)
+
+    return Response({"data": "nope"})
