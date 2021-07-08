cc4f263835a26181b60b4a53647f6d02dded11dd
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-07-07 22:15:45 -0400

    serializing user & property models

--- server/api/serializer.py
+++ server/api/serializer.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Property
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = ["id", "username"]
+
+
+class PropertySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Property
         fields = "__all__"
