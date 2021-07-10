26d387423cb85b264e8f6deeb775058a25de396b
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-07-09 22:10:41 -0400

    deleted testing view

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import testing, get_properties_data
+from .views import get_properties_data
 
 urlpatterns = [
     path("testing", get_properties_data),
-    path("test", testing)
 ]
