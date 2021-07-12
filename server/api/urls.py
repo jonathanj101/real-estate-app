66cbf8563b13a50fb1dbc517d7ca70bfd8471baa
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-07-11 23:56:48 -0400

    updated url

--- server/api/urls.py
+++ server/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import get_properties_data
 
 urlpatterns = [
-    path("testing", get_properties_data),
+    path("show-properties", get_properties_data),
 ]
