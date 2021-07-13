a450f89f287e8643994ca25fa20267131d15bf7e
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-07-12 23:12:05 -0400

    added add_property

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_properties_data
+from .views import get_properties_data, add_property
 
 urlpatterns = [
     path("show-properties", get_properties_data),
+    path("add_property", add_property),
 ]
