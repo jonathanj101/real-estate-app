6e284281244297c7509811ee81425b15046a18db
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-07-14 21:47:48 -0400

    cleaning code a bit

--- server/client/src/components/Navigation.js
+++ server/client/src/components/Navigation.js
@@ -22,7 +22,6 @@ function Navigation() {
     const redirectToPage = (e) => {
         const page = e.currentTarget.children[0].children[0].attributes[4].nodeValue;
         history.push(`${page}`);
-        fetchingData(page);
     };
 
     // const fetchingData = (route) => {
