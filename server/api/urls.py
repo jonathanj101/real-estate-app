0c632a0af9cb2f5dea397ef678b9b65ab17a1291
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-07-14 21:45:35 -0400

    path to favorites-properties or properties saved

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_properties_data, add_property
+from .views import get_properties_data, add_property, favorites_properties
 
 urlpatterns = [
     path("show-properties", get_properties_data),
     path("add_property", add_property),
+    path("favorites-properties", favorites_properties),
 ]
