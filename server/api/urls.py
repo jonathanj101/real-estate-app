e20192e8dae746889e1a158390c9e9aa7c6c598e
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-07-16 00:05:11 -0400

    figuring how to display images of props or take user to virtualTour

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import get_properties_data, add_property, favorites_properties
+from .views import get_properties_data, add_property, favorites_properties, get_property_images_from_zillow, testing_virtual_tour
 
 urlpatterns = [
     path("show-properties", get_properties_data),
     path("add_property", add_property),
     path("favorites-properties", favorites_properties),
+    path("get-properties-images", get_property_images_from_zillow),
+    path("test_virtual", testing_virtual_tour),
 ]
