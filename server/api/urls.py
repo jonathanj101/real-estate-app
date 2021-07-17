7fbeaa04a77d209f6bd6871c09b09a936d2eb384
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-07-16 23:06:01 -0400

    added test_virtual,  get-images

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import get_properties_data, add_property, favorites_properties, testing_virtual_tour
+from .views import get_properties_data, add_property, favorites_properties, get_virtual_tour_url, get_property_images
 
 urlpatterns = [
     path("show-properties", get_properties_data),
     path("add_property", add_property),
     path("favorites-properties", favorites_properties),
-    path("test_virtual", testing_virtual_tour),
+    path("test_virtual", get_virtual_tour_url),
+    path("get-images", get_property_images),
 ]
