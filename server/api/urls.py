32ff5135e106b43aaa1901b99e6b6aeba3e13348
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-07-18 22:41:56 -0400

    added api-key, to return google map api to client

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import get_properties_data, add_property, favorites_properties, get_virtual_tour_url, get_property_images
+from .views import get_properties_data, add_property, favorites_properties, get_virtual_tour_url, get_property_images, google_api_key
 
 urlpatterns = [
     path("show-properties", get_properties_data),
@@ -7,4 +7,5 @@ urlpatterns = [
     path("favorites-properties", favorites_properties),
     path("test_virtual", get_virtual_tour_url),
     path("get-images", get_property_images),
+    path("api-key", google_api_key),
 ]
