67882137af1fead886de0506c46103a4f55abbbb
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-07-19 23:07:02 -0400

    separating google map to its dir

--- server/client/src/components/Map/Google-Map/GoogleMap.js
+++ server/client/src/components/Map/Google-Map/GoogleMap.js
@@ -1,53 +1,78 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import GoogleMapReact from "google-map-react";
-import { makeStyles } from "@material-ui/core";
+import { BottomNavigation, makeStyles } from "@material-ui/core";
 import Loading from "../../LoadingComponent/Loading";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 const GoogleMap = () => {
     const [center, setCenter] = useState({ lat: 59.955413, lng: 30.337844 });
     const [zoom, setZoom] = useState(11);
-    const [googleApiKey, setGoogleApiKey] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
+    const [googleApiKey, setGoogleApiKey] = useState();
+    const [isLoading, setIsLoading] = useState(true);
 
     const classes = styles();
 
     useEffect(() => {
-        if (googleApiKey == "") {
-            setIsLoading(true);
-            fetchAPIKey();
-            console.log(googleApiKey);
-        } else {
+        debugger;
+        // if (isLoading) {
+        const fetchAPIKey = async () => {
             setIsLoading(false);
-            console.log(googleApiKey);
-        }
-    }, [googleApiKey]);
+            const response = await fetch("api/api-key");
+            const { data } = await response.json();
+            console.log(data);
+            setGoogleApiKey(googleApiKey);
+        };
+        fetchAPIKey();
+        // console.log(fetchAPIKey());
+        // } else {
+        //     console.log("not yet");
+        //     return;
+        // }
+        // debugger;
+        // if (isLoading) {
+        //     setIsLoading(true);
+        //     fetchAPIKey();
+        //     //     console.log(googleApiKey);
+        // } else {
+        //     // setIsLoading(false);
+        //     //     console.log(googleApiKey);
+        // }
+    });
 
-    const fetchAPIKey = () => {
-        fetch("api/api-key")
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                setGoogleApiKey(data.data);
-            });
-        console.log(googleApiKey);
-    };
+    // const fetchAPIKey = async () => {
+    //     // setIsLoading(false);
+    //     const response = await axios.get("api/api-key");
+    //     const apikey = response.data.data;
+    //     console.log(apikey);
+    //     // setIsLoading(false);
+    //     setGoogleApiKey(apikey);
+    //     // setGoogleApiKey(apikey)
+    //     // fetch("api/api-key")
+    //     //     .then((response) => response.json())
+    //     //     .then((data) => {
+    //     //         console.log(data);
+    //     //         setGoogleApiKey(data.data);
+    //     //     });
+    //     // console.log(googleApiKey);
+    // };
 
     return (
         <div className={classes.map}>
-            {isLoading ? (
+            {/* {isLoading ? (
                 <Loading />
-            ) : (
-                <GoogleMapReact bootstrapURLKeys={{ key: googleApiKey }} defaultCenter={center} defaultZoom={zoom}>
-                    <AnyReactComponent
+            ) : ( */}
+
+            <GoogleMapReact bootstrapURLKeys={{ key: googleApiKey }} defaultCenter={center} defaultZoom={zoom}>
+                {/* <AnyReactComponent
                         // style={{ fontSize: "50rem", height: "250px", width: "250px" }}
                         lat={59.955413}
                         lng={30.337844}
                         text="My Marker"
-                    />
-                </GoogleMapReact>
-            )}
+                    /> */}
+            </GoogleMapReact>
+            {/* )} */}
         </div>
     );
 };
