c9d4322383d255ad8351f92ce47579b667571bc2
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-07-21 22:31:17 -0400

    displaying property and locationMaker on click

--- server/client/src/components/PropertyModal.js
+++ server/client/src/components/PropertyModal.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Modal, Card, CardContent, CardMedia, Typography, Button, makeStyles } from "@material-ui/core";
 import Loading from "./LoadingComponent/Loading";
+import GoogleMap from "./Map/Google-Map/GoogleMap";
 
 const PropertyModal = ({
     open,
@@ -18,75 +19,88 @@ const PropertyModal = ({
     imagesList,
     image,
     zpid,
+    googleApiKey,
 }) => {
     const [propertyImagesForHomePage, setHomePagePropertyImages] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const classes = styles();
+
+    console.log(latitude, longitude);
 
     useEffect(() => {
+        console.log(isLoading);
         if (zpid !== undefined && zpid !== "") {
-            fetchPropertyImages(zpid);
-            setIsLoading(false);
+            fetchPropertyImages();
         } else {
             console.log("not reading it");
         }
     }, [zpid]);
 
-    const fetchPropertyImages = async (zpid) => {
-        const response = await fetch("api/get-images", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                zpid: zpid,
-            }),
-        });
-        const data = await response.json();
-        console.log(data);
-        // setHomePagePropertyImages(response.data.data);
+    const fetchPropertyImages = async () => {
+        try {
+            setIsLoading(true);
+            const response = await fetch("api/get-images", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    zpid: zpid,
+                }),
+            });
+            const data = await response.json();
+            setHomePagePropertyImages(data.data);
+            setIsLoading(false);
+        } catch (error) {
+            console.log(error);
+        }
     };
 
-    // const imagesListComponent = () => {
-    //     return (
-    //         <CardContent>
-    //             <div className={classes.imagesListDiv}>
-    //                 {/* {imagesList.map((image) => { */}
-    //                 {propertyImagesForHomePage.map((image) => {
-    //                     return <img src={image} className={classes.imagesStyles} />;
-    //                 })}
-    //             </div>
-    //         </CardContent>
-    //     );
-    // };
+    const imagesListComponent = propertyImagesForHomePage.map((image) => {
+        return <img src={image} className={classes.imagesStyles} />;
+    });
 
-    const classes = styles();
     return (
         <div>
             <Modal
-                style={{ width: "75%", margin: "auto" }}
+                style={{ width: "90%", margin: "auto" }}
                 open={open}
                 onClose={handleClose}
                 aria-labelledby="simple-modal-title"
                 aria-describedby="simple-modal-description"
             >
-                <Card>
-                    {/* {isLoading ? <Loading /> : imagesListComponent} */}
-
-                    <CardContent>
-                        <Typography variant="h3">${price}</Typography>
-                        <Typography variant="h5">{address}</Typography>
-                        <Typography variant="h5">
-                            {bathrooms} ba | {bedrooms} bds
-                        </Typography>
-                        <Typography variant="h5">{propertyType}</Typography>
-                        <Typography variant="h5">
-                            {lotAreaValue} {lotAreaUnit}
-                        </Typography>
-                    </CardContent>
-                    <CardContent>map component here</CardContent>
-                    <CardContent>
-                        <Button target="_blank" href={viewTourUrl}>
-                            view tour
-                        </Button>
-                    </CardContent>
+                <Card style={{ height: "95%" }}>
+                    <div style={{ height: "100%", display: "flex", flexDirection: "row-reverse" }}>
+                        <div style={{ width: "30%" }}>
+                            <CardContent style={{ height: "75%" }}>
+                                <div className={classes.imagesListDiv}>
+                                    {isLoading ? <Loading /> : imagesListComponent}
+                                </div>
+                            </CardContent>
+                            <CardContent>
+                                <Typography variant="h3">${price}</Typography>
+                                <Typography variant="h5">{address}</Typography>
+                                <Typography variant="h5">
+                                    {bathrooms} ba | {bedrooms} bds
+                                </Typography>
+                                <Typography variant="h5">{propertyType}</Typography>
+                                <Typography variant="h5">
+                                    {lotAreaValue} {lotAreaUnit}
+                                </Typography>
+                                <Button target="_blank" href={viewTourUrl}>
+                                    view tour
+                                </Button>
+                            </CardContent>
+                        </div>
+                        <div style={{ width: "100%" }}>
+                            <CardContent style={{ height: "100%", width: "100%" }}>
+                                <GoogleMap
+                                    googleApiKey={googleApiKey}
+                                    isLoading={isLoading}
+                                    latitude={latitude}
+                                    longitude={longitude}
+                                />
+                            </CardContent>
+                        </div>
+                    </div>
                 </Card>
             </Modal>
         </div>
@@ -96,12 +110,12 @@ const PropertyModal = ({
 const styles = makeStyles({
     imagesListDiv: {
         width: "100%",
-        height: "500px",
+        height: "100%",
         overflow: "auto",
     },
     imagesStyles: {
         height: "300px",
-        width: "300px",
+        width: "inherit",
     },
 });
 
