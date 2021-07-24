d77a0c9449a2ea4ce624843352be6c3dd627319b
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-07-23 22:59:38 -0400

    url for registration added

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import get_properties_data, add_property, favorites_properties, get_virtual_tour_url, get_property_images, google_api_key
+from .views import get_properties_data, add_property, favorites_properties, get_virtual_tour_url, get_property_images, google_api_key, registration
 
 urlpatterns = [
     path("show-properties", get_properties_data),
@@ -8,4 +8,5 @@ urlpatterns = [
     path("test_virtual", get_virtual_tour_url),
     path("get-images", get_property_images),
     path("api-key", google_api_key),
+    path("registration", registration)
 ]
