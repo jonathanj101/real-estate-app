080a48e6dabdab49a9d5f8a338ff6de7f3aaa416
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-07-25 23:53:01 -0400

    started to implement log in component, not done

--- server/client/src/components/User-Auth/Log-In/LogIn.js
+++ server/client/src/components/User-Auth/Log-In/LogIn.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Button, TextField, Modal } from "@material-ui/core";
 
 const LogIn = () => {
-    return <div></div>;
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [isUsernameValidated, setIsUsernameValidated] = useState("");
+    const [isPasswordValidated, setIsPasswordValidated] = useState("");
+
+    return (
+        <div style={{ width: "100%", height: "100%", margin: "0 auto 250px auto" }}>
+            {/* <div style={{ border: "2px solid black", textAlign: "center" }}> */}
+            <Modal open={true} style={{ border: "2px solid black", textAlign: "center" }}>
+                <form style={{ width: "10%", margin: "250px auto", border: "2px solid red", backgroundColor: "white" }}>
+                    <div>
+                        <h2>Sign In</h2>
+                    </div>
+                    <div className="form-row">
+                        <TextField
+                            style={{ margin: "25px auto" }}
+                            id="username"
+                            label="Username"
+                            error={isUsernameValidated}
+                            value={username}
+                            helperText={isUsernameValidated ? "Username should not be empty" : ""}
+                            onChange={(e) => setUsername(e.currentTarget.value)}
+                        />
+                    </div>
+                    <div className="form-row">
+                        <TextField
+                            style={{ margin: "25px auto" }}
+                            id="password"
+                            label="Password"
+                            error={isPasswordValidated}
+                            value={password}
+                            helperText={isPasswordValidated ? "Username should not be empty" : ""}
+                            onChange={(e) => setPassword(e.currentTarget.value)}
+                        />
+                    </div>
+                    <div className="register-option-container" style={{ margin: "0 auto 25px auto" }}>
+                        <span>Don't have an account?</span>
+                        <a href="/register">Register here!</a>
+                    </div>
+                </form>
+            </Modal>
+            {/* </div> */}
+        </div>
+    );
 };
 
 export default LogIn;
