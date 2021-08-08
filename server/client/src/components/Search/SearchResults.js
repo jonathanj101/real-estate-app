b241a2ff4ae6f7620506e1c76ebbd539473d1449
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-08-07 23:02:45 -0400

    will show search results here

---
+++ server/client/src/components/Search/SearchResults.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import SearchComponent from "./SearchComponent";
+
+const SearchResults = () => {
+    return (
+        <div>
+            <SearchComponent />
+        </div>
+    );
+};
+
+export default SearchResults;
