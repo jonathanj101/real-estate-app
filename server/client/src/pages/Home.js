67419cd4297c5ec322e0f1d89ccc5fb90b9b4422
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-08-07 23:04:59 -0400

    cleaning up code a bit

--- server/client/src/pages/Home.js
+++ server/client/src/pages/Home.js
@@ -1,25 +1,14 @@
 import React, { useState } from "react";
-import { makeStyles, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 import Properties from "../components/Properties";
-import SearchComponent from "../components/SearchComponent";
+import SearchComponent from "../components/Search/SearchComponent";
 
 function Home({ googleApiKey }) {
-    // const [arr, setArr] = useState([]);
-
-    // const fetchingData = () => {
-    //     fetch("/testing")
-    //         .then((response) => response.json())
-    //         .then((data) => {
-    //             console.log(data.data);
-    //             setArr(data.data.props);
-    //         });
-    // };
     const classes = styles();
     return (
         <div id="home js">
             <SearchComponent />
             <Properties googleApiKey={googleApiKey} />
-            {/* <Button onClick={fetchingData}>click here</Button> */}
         </div>
     );
 }
