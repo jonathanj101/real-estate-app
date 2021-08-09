305e9f4d53da60cdaf2e306fad5e243af64702b4
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-08-08 22:48:27 -0400

    cleaning up copde

--- server/client/src/pages/Home.js
+++ server/client/src/pages/Home.js
@@ -1,18 +1,12 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core";
+import React from "react";
 import Properties from "../components/Properties";
-import SearchComponent from "../components/Search/SearchComponent";
 
 function Home({ googleApiKey }) {
-    const classes = styles();
     return (
         <div id="home js">
-            <SearchComponent />
             <Properties googleApiKey={googleApiKey} />
         </div>
     );
 }
-
-const styles = makeStyles({});
 
 export default Home;
