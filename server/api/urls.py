6d0e86a1cb36245ec2cc0a7f1d4d2b3755328f07
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-08-09 21:58:05 -0400

    added path for search/<str:location>

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import get_properties_data, add_property, favorites_properties, get_virtual_tour_url, get_property_images, google_api_key, registration, log_in, verify_user, delete_property
+from .views import get_properties_data, add_property, favorites_properties, get_virtual_tour_url, get_property_images, google_api_key, registration, log_in, verify_user, delete_property, search_properties
 
 urlpatterns = [
     path("show-properties", get_properties_data),
@@ -12,4 +12,5 @@ urlpatterns = [
     path("log-in", log_in),
     path("verify-user", verify_user),
     path("delete-property", delete_property),
+    path("search/<str:location>", search_properties)
 ]
