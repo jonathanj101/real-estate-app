70256e248944a530ffb16af18fd0e794ef9b4632
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-08-15 20:13:47 -0400

    cleaning up code, moved to its dir and stylings as well

---
+++ server/client/src/components/Search/Search-Component/SearchComponentStyles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core";
+
+const styles = makeStyles({
+    formControlStyles: {
+        display: "flex",
+        justifyContent: "center",
+        width: 500,
+        margin: "10px auto",
+    },
+});
+
+export default styles;
