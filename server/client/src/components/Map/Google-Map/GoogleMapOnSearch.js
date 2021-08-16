3aa95dc222023098aebbea6a2135b10f2595efef
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-08-15 20:08:27 -0400

    cleaning up code, moved to its dir and stylings as well

---
+++ server/client/src/components/Map/Google-Map/GoogleMapOnSearch.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import GoogleMapReact from "google-map-react";
+import Loading from "../../LoadingComponent/Loading";
+import LocationMarker from "../Location-Marker/LocationMarker";
+import styles from "./GoogleMapStyles";
+
+const GoogleMap = ({ propertiesData }) => {
+    const [center] = useState({ lat: 40.718848, lng: -73.862117 });
+    const [googleApiKey, setGoogleApiKey] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
+    const classes = styles();
+
+    useEffect(() => {
+        if (googleApiKey === "") {
+            fetchGoogleApiKey();
+        }
+    }, [googleApiKey]);
+
+    const fetchGoogleApiKey = async () => {
+        const response = await axios.get("api/api-key");
+        setGoogleApiKey(response.data.data);
+        setIsLoading(false);
+    };
+
+    const mapPropertiesData = () => {
+        return propertiesData.map((prop) => {
+            return <LocationMarker lat={prop.latitude} lng={prop.longitude} />;
+        });
+    };
+
+    const markers =
+        propertiesData !== undefined && propertiesData.length !== 0 ? (
+            mapPropertiesData()
+        ) : (
+            <LocationMarker lat={center.lat} lng={center.lng} />
+        );
+
+    return (
+        <div className={classes.map}>
+            {isLoading ? (
+                <Loading />
+            ) : (
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: googleApiKey }}
+                    center={{
+                        lat:
+                            propertiesData !== undefined && propertiesData.length !== 0
+                                ? propertiesData[0].latitude
+                                : center.lat,
+                        lng:
+                            propertiesData !== undefined && propertiesData.length !== 0
+                                ? propertiesData[0].longitude
+                                : center.lng,
+                    }}
+                    zoom={10}
+                >
+                    {markers}
+                </GoogleMapReact>
+            )}
+        </div>
+    );
+};
+
+export default GoogleMap;
