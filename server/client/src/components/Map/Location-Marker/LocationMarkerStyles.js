118e4f0591451783a537cb905cdf8739c3874c9c
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-08-15 20:09:06 -0400

    cleaning up code, moved to its dir and stylings as well

---
+++ server/client/src/components/Map/Location-Marker/LocationMarkerStyles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core";
+
+const styles = makeStyles({
+    iconStyles: {
+        color: "red",
+        fontSize: "1.5rem",
+    },
+});
+
+export default styles;
