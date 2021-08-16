bf2aa50e49cf21a10554c484899c06d08263184e
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-08-15 20:12:18 -0400

    cleaning up code, moved to its dir and stylings as well

---
+++ server/client/src/components/Pagination/PaginationStyles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core";
+
+const styles = makeStyles({
+    mainDiv: {
+        width: "50%",
+        margin: "auto",
+    },
+});
+
+export default styles;
