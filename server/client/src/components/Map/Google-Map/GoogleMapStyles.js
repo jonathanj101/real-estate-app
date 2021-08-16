0e231b49541c8e3fd72b9df107fb23448c255b6d
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-08-15 20:08:40 -0400

    cleaning up code, moved to its dir and stylings as well

---
+++ server/client/src/components/Map/Google-Map/GoogleMapStyles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core";
+
+const styles = makeStyles({
+    map: {
+        height: "100%",
+        width: "100%",
+        position: "relative",
+    },
+});
+
+export default styles;
