8cbaab11ccd48fbc185857b4b794ef760f27b1a7
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-08-15 20:14:38 -0400

    cleaning up code, moved to its dir and stylings as well

--- server/client/src/components/User-Auth/Log-In/LogIn.js
+++ server/client/src/components/User-Auth/Log-In/LogIn.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
-import { Button, TextField, Modal, makeStyles } from "@material-ui/core";
+import { Button, TextField, Modal } from "@material-ui/core";
 import AlertMessage from "../../Alert-Message/AlertMessage";
+import styles from "./LogInStyles";
 
 const LogIn = ({ show, handleClose, handleLogIn }) => {
     const [username, setUsername] = useState("");
@@ -116,47 +117,5 @@ const LogIn = ({ show, handleClose, handleLogIn }) => {
         </div>
     );
 };
-
-const styles = makeStyles({
-    mainDiv: {
-        width: "100%",
-        height: "100%",
-    },
-    formDiv: {
-        width: "25%",
-        height: "40%",
-        margin: "250px auto",
-        borderRadius: "rgb(179 178 178) 18px 8px 32px",
-    },
-    modalStyles: {
-        border: "2px solid black",
-        textAlign: "center",
-    },
-    formStyles: {
-        width: "100%",
-        height: "100%",
-        backgroundColor: "white",
-    },
-    formTitleDiv: {
-        width: "15%",
-        margin: "auto",
-    },
-    formTitle: {
-        fontSize: "2rem",
-        borderBottom: "5px solid red",
-    },
-    btnStyles: {
-        color: "red",
-        border: "1px solid red",
-        width: "25%",
-        "&:hover": {
-            backgroundColor: "red",
-            color: "white",
-        },
-    },
-    registerOptDiv: {
-        margin: "25px auto 0 auto",
-    },
-});
 
 export default LogIn;
