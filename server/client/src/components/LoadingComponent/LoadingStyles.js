f2509d684951f704c3a6ac18c546e1d602e692f6
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-08-15 20:07:45 -0400

    cleaning up code, moved to its dir and stylings as well

---
+++ server/client/src/components/LoadingComponent/LoadingStyles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@material-ui/core";
+
+const styles = makeStyles({
+    iconDiv: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+        width: "100%",
+    },
+    icon: {
+        fontSize: "5rem",
+    },
+});
+
+export default styles;
