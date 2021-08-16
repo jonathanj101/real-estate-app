0f441fb7f51f7d667a39f9fe390556bfffd69cd5
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-08-15 20:15:17 -0400

    cleaning up code, moved to its dir and stylings as well

---
+++ server/client/src/components/User-Auth/Registration/RegisterFormStyles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core";
+
+const styles = makeStyles({
+    mainDiv: {
+        width: "100%",
+        height: "100%",
+        margin: "0 auto 250px auto",
+        textAlign: "center",
+    },
+});
+
+export default styles;
