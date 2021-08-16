0aa582b3a3d334b9becbcfe16fcbfdd30015d100
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-08-15 20:11:23 -0400

    cleaning up code, moved to its dir and stylings as well

---
+++ server/client/src/components/Modal/PropertyModalStyles.js
@@ -0,0 +1,20 @@
+import { makeStyles } from "@material-ui/core";
+
+const styles = makeStyles({
+    imagesListDiv: {
+        width: "100%",
+        height: "100%",
+        overflow: "auto",
+    },
+    imagesStyles: {
+        height: "300px",
+        width: "inherit",
+    },
+    tourBtnDiv: {
+        width: "50%",
+        margin: "auto",
+        textAlign: "center",
+    },
+});
+
+export default styles;
