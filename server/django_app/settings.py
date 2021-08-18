b1f7adb8e5eede204891049bfeeec1176e51ecfe
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-08-18 17:15:39 -0400

    attemtping to publish web app

--- server/django_app/settings.py
+++ server/django_app/settings.py
@@ -139,13 +139,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+REACT_APP_PATH = os.path.join(BASE_DIR, "client")
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 print(f"static rooot {STATIC_ROOT}")
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(REACT_APP_PATH, 'static')
 ]
 
 
