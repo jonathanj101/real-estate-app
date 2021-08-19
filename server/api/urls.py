48353449b6b395aff32a1629ada82ecc421b25d3
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-08-18 20:59:49 -0400

    renaming virtual tour url

--- server/api/urls.py
+++ server/api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("show-properties", get_properties_data),
     path("add_property", add_property),
     path("favorites-properties", favorites_properties),
-    path("test_virtual", get_virtual_tour_url),
+    path("get-virtual-tour", get_virtual_tour_url),
     path("get-images", get_property_images),
     path("api-key", google_api_key),
     path("registration", registration),
