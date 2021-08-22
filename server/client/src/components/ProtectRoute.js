74bc312c912427c65ff9563c1a82fa9fa02c5006
jonathanj101 <49576723+jonathanj101@users.noreply.github.com>
2021-08-22 19:35:08 -0400

    clean up code

--- server/client/src/components/ProtectRoute.js
+++ server/client/src/components/ProtectRoute.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Redirect } from "react-router";
 
 const ProtectRoute = ({ component: Component, isUserAuthenticated }) => {
+    debugger;
+    console.log(Component);
+    console.log(isUserAuthenticated);
     return isUserAuthenticated ? <Component /> : <Redirect to={{ pathname: "/page-not-found" }} />;
 };
 
